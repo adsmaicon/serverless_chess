6dc01b30365c7f871b0eb8ea1efb9b201c489ab2
Maicon Carvalho <m.carvalho@vhsys.com.br>
2020-11-06 09:25:02 -0300

    :turtle:

--- chalicelib/model/board.py
+++ chalicelib/model/board.py
@@ -1,16 +1,37 @@
+"""[summary]
 
+Returns:
+    [type]: [description]
+"""
 
 class Board:
-    
+    """[summary]
+    """
+
     def __init__(self):
+        """[summary]
+        """
         self.square = [
             ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H' ],
             [1, 2, 3, 4, 5, 6, 7, 8 ]
-        ];
+        ]
 
-    
     def square_validator(self, square):
-        return (square is not None 
-        and len(square.strip()) == 2 
+        """[summary]
+
+        Args:
+            square ([type]): [description]
+
+        Returns:
+            [type]: [description]
+        """
+        return (square is not None
+        and len(square.strip()) == 2
         and square[0].upper() in self.square[0]
         and int(square[1]) in self.square[1])
+
+    def position_validator(self, square):
+        """
+        docstring
+        """
+        return square
--- chalicelib/model/piece.py
+++ chalicelib/model/piece.py
@@ -1,8 +1,16 @@
+"""[summary]
 
+Returns:
+    [type]: [description]
+"""
 
 class Piece:
-    
+    """[summary]
+    """
+
     def __init__(self):
+        """[summary]
+        """
         self.codes = {
             'K': "King",
             "Q": "Queen",
@@ -11,6 +19,20 @@ class Piece:
             "N": "Knight",
             "": "Pawn"
         }
-    
+
     def code_validator(self, code):
+        """[summary]
+
+        Args:
+            code ([type]): [description]
+
+        Returns:
+            [type]: [description]
+        """
         return code.strip() in self.codes
+
+    def position_validator(self, square):
+        """
+        docstring
+        """
+        return square
