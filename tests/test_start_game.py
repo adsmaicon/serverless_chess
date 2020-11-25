7079865624062c13469480e928f6d399b914064e
ALISSON PELIZARO <a.pelizaro@vhsys.com.br>
2020-11-25 18:39:55 -0300

    Adiciona testes de validaçao do Header Location e UUID do jogo

--- tests/test_start_game.py
+++ tests/test_start_game.py
@@ -10,10 +10,45 @@ def test_client():
         yield client
 
 
-def test_primeira_jogada(test_client):
-    result = test_client.http.post("/jogo/novo", headers={"Content-type": "application/json"}, 
-    body=json.dumps({
-        'nick_name': 'pepo'
+def test_start_game(test_client):
+    result = test_client.http.post(
+        "/jogo/novo",
+        headers={
+            "Content-type": "application/json"
+        },
+        body=json.dumps({
+            'nick_name': 'pepo'
         })
     )
     assert result.status_code == 201
+
+
+def test_start_game_header_location(test_client):
+    result = test_client.http.post(
+        "/jogo/novo",
+        headers={
+            "Content-type": "application/json"
+        },
+        body=json.dumps({
+            'nick_name': 'Alisson'
+        })
+    )
+    headers = list(map(lambda x: x.lower(), result.headers))
+    assert 'location' in headers
+
+
+def test_start_game_get_game_id(test_client):
+    result = test_client.http.post(
+        "/jogo/novo",
+        headers={
+            "Content-type": "application/json"
+        },
+        body=json.dumps({
+            'nick_name': 'Alisson'
+        })
+    )
+
+    headers = {k.lower(): v for k, v in result.headers.items()}
+    uriSplitted = headers['location'].split('/')
+
+    assert len(uriSplitted) > 3 
